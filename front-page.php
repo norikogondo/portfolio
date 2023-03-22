ffe9853f136a5c3a2e41332e541f28444bd8ef7f
Shuhei Shagawa <sgw.sgw.shu@gmail.com>
2023-03-21 21:07:31 -0700

    [update] front-page text

--- front-page.php
+++ front-page.php
@@ -1,34 +1,34 @@
 <?php get_header(); ?>
-
 <section class="hero-header">
     <div class="hero-header-content">
         <span id='typeAnimation'></span>
     </div>
     <p class="hero-jp">デザインの力であなたのビジネスに貢献します。</p>
     <p class="hero-message">
-        I'm a UX/UI designer in Canada and Japan, passionate about contributing to your business
-        with creative design, identities and experiences.
+        I'm a UX/UI Designer and a Product Designer in Canada and Japan, passionate about contributing to your business
+        through creative designs, branding, and user experiences.
     </p>
 </section>
 
 <section class="about">
     <div class="about-wrap">
-        <div class="about-img"><img src="<?php echo get_template_directory_uri(); ?>/public/images/about-img.jpg" alt="ABOUT">
+        <div class="about-img"><img src="<?php echo get_template_directory_uri(); ?>/public/images/about-img.jpg"
+                alt="ABOUT">
         </div>
         <div class="about-info">
             <h2>ABOUT</h2>
             <div class="about-text">
                 <p>
-                    Earned my Bachelor's Degree in economics. Through a variety of work experiences, I noticed that both
-                    user interface and user experience design could play a crucial role for any type of businesses to
-                    succeed in contemporary society. That is the biggest reason why I switched my career to a web
-                    designer in 2016, and have worked on websites, app design, banners, ads, and graphics for small and
-                    mid-sized companies in Japan, as well as in Canada recently. I have been driven to digital design,
-                    and currently pursuing my career as a UX/UI designer.</p>
-
+                    I have earned a Bachelor's degree in Economics. Through various work experiences, I have come to
+                    realize that user interface and user experience design can potentially play a crucial role in the
+                    success of any type of business in contemporary society. That is the main reason why I switched to a
+                    career in UX/UI design in 2016, and since then, I have been involved in creating websites, apps,
+                    graphics, and digital marketing for small and mid-sized companies in Japan and Canada. I am driven
+                    by digital design and am currently pursuing my career as a UX/UI designer or a product designer.</p>
+                ​
             </div>
         </div>
-
+        ​
     </div>
 </section>
 
@@ -40,6 +40,7 @@
         'tag_slug__and' => array('favorite',),
         'orderby'        => 'menu_order date',
         'order'          => 'DESC',
+
     );
 
     $my_query = new WP_Query($args);
@@ -59,8 +60,8 @@
 <section class="contact">
     <div class="contact-wrap">
         <h2>CONTACT</h2>
-        <p>Ask me anything or let me know an idea you would like to collaborate with me.</p>
-        <p>I will get back to you ASAP.</p>
+        <p>Feel free to ask me anything or let me know any ideas you would like to collaborate on.</p>
+        <p>I will get back to you as soon as possible.</p>
         <a href="<?php the_permalink(21); ?>" class="button">CONTACT</a>
     </div>
 </section>
